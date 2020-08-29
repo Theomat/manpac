88ab10d5faaeb0743549e4a616c5b6eb0127fce5
Theomat <theomatricon@gmail.com>
2020-08-29 20:49:19 +0200

    Improved documentation

--- manpac/modifiers/abstract_modifier.py
+++ manpac/modifiers/abstract_modifier.py
@@ -7,6 +7,13 @@ from abc import ABC
 class AbstractModifier(ABC):
     """
     Represents an abstract modifier.
+
+    Parameters
+    -----------
+    - *game*: (**Game**)
+        the game this modifier is being used in
+    - *duration*: (**float**)
+        the number of ticks on use this modifier will last
     """
 
     def __init__(self, game, duration):
@@ -19,42 +26,52 @@ class AbstractModifier(ABC):
     def speed_multiplier(self):
         """
         Speed multiplier conferred by this modifier.
+        type: **float**
         """
         return 1
 
     @property
     def is_tangible(self):
         """
-        Whether this modifier makes this entity intangible (i.e. can walk through wall) or not.
+        Whether this modifier makes entities intangible (i.e. can walk through wall) or not.
+        type: **bool**
         """
         return True
 
     @property
     def can_ghost_collide(self):
         """
-        Whether this modifier makes this ghost able to collide with other ghosts.
+        Whether this modifier makes a ghost able to collide with other ghosts.
+        type: **bool**
         """
         return False
 
     @property
     def alive(self):
         """
-        Whether this modifier is alive or not.
+        Whether this modifier is active or not.
+        type: **bool**
         """
         return not self.used or self.remaining_duration > 0
 
     def use(self, entity):
+        """
+        Use this modifier on the specified entity.
+
+        Parameters
+        -----------
+        - *entity*: (**Entity**)
+            the entity this modifier is being used on
+        """
         self.used = True
         self.on_use(entity)
 
     def on_use(self, entity):
         """
-        Fired when this modifier is used on entity.
+        Fired when this modifier is used on an entity.
 
         Parameters
         -----------
-        - *game*: (**Game**)
-            the game it is being used in
         - *entity*: (**Entity**)
             the entity this modifier is being used on
         """
