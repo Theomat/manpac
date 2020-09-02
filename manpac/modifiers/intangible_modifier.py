58923b1b3091102291563a8d7cbd07698c8f5762
Theomat <theomatricon@gmail.com>
2020-09-02 21:35:15 +0200

    Added teleport back inside walkable cells

--- manpac/modifiers/intangible_modifier.py
+++ manpac/modifiers/intangible_modifier.py
@@ -1,5 +1,6 @@
 from manpac.utils import export
 from manpac.modifiers.abstract_modifier import AbstractModifier
+from manpac.direction import Direction
 
 
 @export
@@ -10,3 +11,12 @@ class IntangibleModifier(AbstractModifier):
     @property
     def is_tangible(self):
         return False
+
+    def on_death(self, entity):
+        super(IntangibleModifier, self).on_death(entity)
+        if not entity.is_tangible:
+            return
+        new_pos = self.game.map.closest_walkable(entity.pos) + .5
+        for direction in Direction.representing(new_pos - entity.pos):
+            new_pos -= direction.vector * (.5 - entity.size)
+        entity.teleport(new_pos)
