8863fba8a2f4851612e4a25db6a6c698a10117f2
Theomat <theomatricon@gmail.com>
2020-09-02 18:27:17 +0200

    Refactored to use mostly building blocks

--- manpac/maps/map_pacman.py
+++ manpac/maps/map_pacman.py
@@ -3,88 +3,88 @@ from manpac.cell import Cell
 from manpac.map import Map
 from manpac.modifiers.speed_modifier import SpeedModifier
 from manpac.boost_generators.simple_boost_generator import SimpleBoostGenerator
+import manpac.maps.map_builder as build
 
 import numpy as np
 
-def man_pacman(game):
-    boost_generator = SimpleBoostGenerator(game,1,[(1,lambda:SpeedModifier(game,10,2))],[(1,lambda:SpeedModifier(game,10,2))])
-    map = Map((21,28),boost_generator=boost_generator)
 
-    for i in range(21):
-        map[np.array([i,0])] = Cell.WALL
-        map[np.array([i,27])] = Cell.WALL
-    for i in range(9):
-        map[np.array([0,i+1])] = Cell.WALL
-        map[np.array([20,i+1])] = Cell.WALL
-        map[np.array([4,i+9])] = Cell.WALL
-        map[np.array([16,i+9])] = Cell.WALL
-        map[np.array([0,i+18])] = Cell.WALL
-        map[np.array([20,i+18])] = Cell.WALL
-    for i in (0,4,10,14):
-        map[np.array([2+i,2])] = Cell.WALL
-        map[np.array([2+i,3])] = Cell.WALL
-        map[np.array([2+i,4])] = Cell.WALL
-        map[np.array([3+i,4])] = Cell.WALL
-        map[np.array([3+i,2])] = Cell.WALL
-        map[np.array([4+i,4])] = Cell.WALL
-        map[np.array([4+i,3])] = Cell.WALL
-        map[np.array([4+i,2])] = Cell.WALL
-    for i in range(1,5):
-        map[np.array([10,i])] = Cell.WALL
-        map[np.array([i,9])] = Cell.WALL
-        map[np.array([i+16,9])] = Cell.WALL
-        map[np.array([i,18])] = Cell.WALL
-        map[np.array([i+15,18])] = Cell.WALL
-        map[np.array([6,14+i])] = Cell.WALL
-        map[np.array([14,14+i])] = Cell.WALL
-    for (i,j) in ((2,6),(16,6),(0,22),(18,22)):
-        map[np.array([i,j])] = Cell.WALL
-        map[np.array([i+1,j])] = Cell.WALL
-        map[np.array([i+2,j])] = Cell.WALL
-        map[np.array([i,j+1])] = Cell.WALL
-        map[np.array([i+1,j+1])] = Cell.WALL
-        map[np.array([i+2,j+1])] = Cell.WALL
-    for j in (6,17,22):
-        map[np.array([8,j])] = Cell.WALL
-        map[np.array([9,j])] = Cell.WALL
-        map[np.array([10,j])] = Cell.WALL
-        map[np.array([11,j])] = Cell.WALL
-        map[np.array([12,j])] = Cell.WALL
-        map[np.array([8,j+1])] = Cell.WALL
-        map[np.array([9,j+1])] = Cell.WALL
-        map[np.array([10,j+1])] = Cell.WALL
-        map[np.array([11,j+1])] = Cell.WALL
-        map[np.array([12,j+1])] = Cell.WALL
-    for i in range(3):
-        map[np.array([6,6+i])] = Cell.WALL
-        map[np.array([6+i,9])] = Cell.WALL
-        map[np.array([6,10+i])] = Cell.WALL
-        map[np.array([14,6+i])] = Cell.WALL
-        map[np.array([12+i,9])] = Cell.WALL
-        map[np.array([14,10+i])] = Cell.WALL
-        map[np.array([6+i,20])] = Cell.WALL
-        map[np.array([2+i,20])] = Cell.WALL
-        map[np.array([16+i,20])] = Cell.WALL
-        map[np.array([12+i,20])] = Cell.WALL
-        map[np.array([4,21+i])] = Cell.WALL
-        map[np.array([16,21+i])] = Cell.WALL
-        map[np.array([10,23+i])] = Cell.WALL
-        map[np.array([14,22+i])] = Cell.WALL
-        map[np.array([6,22+i])] = Cell.WALL
-        map[np.array([10,18+i])] = Cell.WALL
-        map[np.array([10,7+i])] = Cell.WALL
-    for i in range(7):
-        map[np.array([2+i,25])] = Cell.WALL
-        map[np.array([12+i,25])] = Cell.WALL
-    for i in range(5):
-        map[np.array([8+i,15])] = Cell.WALL
-        map[np.array([8,11+i])] = Cell.WALL
-        map[np.array([12,11+i])] = Cell.WALL
-    map[np.array([9,11])] = Cell.WALL
-    map[np.array([11,11])] = Cell.WALL
-    map[np.array([6,14])] = Cell.WALL
-    map[np.array([14,14])] = Cell.WALL
-    map.spawns[EntityType.GHOST] = np.array([10.5,10.5])
-    map.spawns[EntityType.PACMAN] = np.array([10, 14])
+class MapPacman(Map):
 
-    return map
+    def __init__(self, game):
+        ghost_factory = [(1, lambda:SpeedModifier(game, 3 * 60, 2))]
+        pacman_factory = [(1, lambda:SpeedModifier(game, 3 * 60, 2))]
+        boost_generator = SimpleBoostGenerator(game, .01, ghost_factory, pacman_factory)
+        super(MapPacman, self).__init__((21, 28), boost_generator)
+
+        # Borders of the map
+        build.block(self, 0, 0, 21, 1)
+        build.block(self, 0, 27, 21, 1)
+
+        build.block(self, 0, 1, 1, 9)
+        build.block(self, 20, 1, 1, 9)
+        build.block(self, 4, 9, 1, 9)
+        build.block(self, 16, 9, 1, 9)
+        build.block(self, 0, 18, 1, 9)
+        build.block(self, 20, 18, 1, 9)
+
+        # Border part that links top and bottom part of the map
+        build.block(self, 16, 9, 5, 10)
+        build.block(self, 0, 9, 5, 10)
+
+        # Thing the comes inwards at the center at the top
+        build.block(self, 10, 1, 1, 4)
+
+        # Top Blocks 3x3
+        for i in (0, 4, 10, 14):
+            build.block(self, 2+i, 2, 3, 3)
+
+        # Vertical line around the center
+        build.block(self, 6, 14, 1, 5)
+        build.block(self, 14, 14, 1, 5)
+
+        # Small top blocks
+        build.block(self, 2, 6, 3, 2)
+        build.block(self, 16, 6, 3, 2)
+
+        # Blocks that come back inside at the bottom of the map on the sides
+        build.block(self, 1, 22, 2, 2)
+        build.block(self, 18, 22, 2, 2)
+
+        # Horizontal part of the T
+        for j in (6, 17, 22):
+            build.block(self, 8, j, 5, 2)
+
+        for i in range(3):
+            self[6, 6+i] = Cell.WALL
+            self[6+i, 9] = Cell.WALL
+            self[6, 10+i] = Cell.WALL
+            self[14, 6+i] = Cell.WALL
+            self[12+i, 9] = Cell.WALL
+            self[14, 10+i] = Cell.WALL
+            self[6+i, 20] = Cell.WALL
+            self[2+i, 20] = Cell.WALL
+            self[16+i, 20] = Cell.WALL
+            self[12+i, 20] = Cell.WALL
+            self[4, 21+i] = Cell.WALL
+            self[16, 21+i] = Cell.WALL
+            self[10, 23+i] = Cell.WALL
+            self[14, 22+i] = Cell.WALL
+            self[6, 22+i] = Cell.WALL
+            self[10, 18+i] = Cell.WALL
+            self[10, 7+i] = Cell.WALL
+
+        # Bottom horizontal lines
+        build.block(self, 2, 25, 7, 1)
+        build.block(self, 12, 25, 7, 1)
+
+        # Central block
+        build.block(self, 8, 15, 5, 1)
+        build.block(self, 8, 11, 1, 5)
+        build.block(self, 12, 11, 1, 5)
+
+        # Last walls to finish cell
+        self[9, 11] = Cell.WALL
+        self[11, 11] = Cell.WALL
+
+        self.spawns[EntityType.GHOST] = np.array([10, 10], dtype=np.int)
+        self.spawns[EntityType.PACMAN] = np.array([10, 16], dtype=np.int)
