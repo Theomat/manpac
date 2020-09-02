72b16c0a423d750db2aaa59cc81f9ead63fc168d
Theomat <theomatricon@gmail.com>
2020-09-02 18:29:15 +0200

    Added boost use

--- manpac/controllers/human_controller.py
+++ manpac/controllers/human_controller.py
@@ -1,8 +1,8 @@
 import pygame
 from manpac.utils import export
 from manpac.direction import Direction
-from manpac.entity_type import EntityType
 from manpac.controllers.abstract_controller import AbstractController
+
 
 @export
 class HumanController(AbstractController):
@@ -15,24 +15,28 @@ class HumanController(AbstractController):
         the game this controller is being used in
     """
 
-    def __init__(self,game):
-        super(HumanController,self).__init__(game)
+    def __init__(self, game):
+        super(HumanController, self).__init__(game)
 
-    def update(self,ticks):
-        my_direction = 0
+    def update(self, ticks):
+        my_direction = None
         #  List all the key and the pressed key are true
         L = pygame.key.get_pressed()
         if L[pygame.K_RIGHT]:
             my_direction = Direction.RIGHT
-        if L[pygame.K_LEFT]:
+        elif L[pygame.K_LEFT]:
             my_direction = Direction.LEFT
-        if L[pygame.K_UP]:
+        elif L[pygame.K_UP]:
             my_direction = Direction.UP
-        if L[pygame.K_DOWN]:
+        elif L[pygame.K_DOWN]:
             my_direction = Direction.DOWN
-        #Indicate the direction and move if a key is pressed
-        if my_direction != 0:
+
+        if L[pygame.K_SPACE]:
+            self.entity.use_modifier()
+        # Indicate the direction and move if a key is pressed
+        if my_direction is not None:
             self.entity.direction = my_direction
             self.entity.moving = True
         else:
             self.entity.moving = False
+        self.game.map.move(self.entity, ticks)
