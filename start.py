9318c3a66ee4d7565656989c3f8b5f021c663a04
Theomat <theomatricon@gmail.com>
2020-09-02 18:35:00 +0200

    Updated to latest changes

--- start.py
+++ start.py
@@ -2,15 +2,11 @@ from manpac.ui.interface import Interface
 from manpac.game import Game
 from manpac.entity_type import EntityType
 from manpac.entity import Entity
-from manpac.cell import Cell
-from manpac.controllers.random_walk_controller import RandomWalkController
 from manpac.controllers.target_seeker_controller import TargetSeekerController
+from manpac.controllers.random_walk_controller import RandomWalkController
 from manpac.controllers.human_controller import HumanController
 from manpac.controllers.walk_away_controller import WalkAwayController
-from manpac.map import Map
-from manpac.maps.map_pacman import *
-
-import numpy as np
+from manpac.maps.map_pacman import MapPacman
 
 
 ghosts = []
@@ -20,23 +16,22 @@ for i in range(4):
 pacman = Entity(EntityType.PACMAN)
 
 game = Game(pacman, *ghosts)
-n = True
+hasHuman = False
+hasRandom = False
 for ghost in ghosts:
-    if n:
+    if not hasHuman:
         ghost.attach(HumanController(game))
-        n = False
+        hasHuman = True
+    elif not hasRandom:
+        hasRandom = True
+        ghost.attach(RandomWalkController(game, 60*2))
     else:
-        ghost.attach(WalkAwayController(game,60*2))
-        #ghost.attach(RandomWalkController(game, 60 * 2))
+        ghost.attach(WalkAwayController(game, 60*2))
+
+# pacman.attach(RandomWalkController(game, 60))
 pacman.attach(TargetSeekerController(game))
 
-#map = Map((40, 40))
-#for i in range(30):
-#    map[np.random.randint(0, 40 - 1, 2)] = Cell.WALL
-#map.spawns[EntityType.GHOST] = np.array([20, 20])
-#map.spawns[EntityType.PACMAN] = np.array([1, 1])
-
-map = man_pacman(game)
+map = MapPacman(game)
 
 interface = Interface(game)
 interface.start(map)
