cde3b9e2721c4731c2b12ae886e6c6d447948665
Theomat <theomatricon@gmail.com>
2020-09-03 20:27:57 +0200

    Fixed rect_border

--- manpac/ui/draw_utils.py
+++ manpac/ui/draw_utils.py
@@ -5,7 +5,7 @@ import pygame
 
 
 @export
-def rect_border(width, height, border, color, fill_color=(0, 0, 0)):
+def rect_border(width, height, border, color):
     """
     Create a rectangle border surface than can now be blit.
 
@@ -19,10 +19,7 @@ def rect_border(width, height, border, color, fill_color=(0, 0, 0)):
         the size of the border in pixels
     - *color*: (**int 3-tuple**)
         the color used to stroke the border
-    - *fill_color*: (**int 3-tuple**)
-        the color used to fill the inside of the rectangle
     """
     surf = pygame.Surface((width + border * 2, height + border * 2), pygame.SRCALPHA)
-    pygame.draw.rect(surf, color, (0, 0, width + border * 2, height + border * 2), 1)
-    pygame.draw.rect(surf, fill_color, (border, border, width, height), 0)
+    pygame.draw.rect(surf, color, (border, border, width, height), border)
     return surf
