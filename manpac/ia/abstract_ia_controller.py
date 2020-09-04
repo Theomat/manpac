44d9f690f59c638466fabdf4f4ead4aa4d9b7dd2
Theomat <theomatricon@gmail.com>
2020-09-04 17:37:41 +0200

    Added AbstractIAController

---
+++ manpac/ia/abstract_ia_controller.py
@@ -0,0 +1,50 @@
+from manpac.controllers.abstract_controller import AbstractController
+from manpac.utils import export
+
+from abc import ABC, abstractmethod
+
+
+@export
+class AbstractIAController(AbstractController, ABC):
+    """
+    An abstract IA controller to easily use with a model.
+    """
+    @abstractmethod
+    def init_state(self):
+        """
+        Fired the first time update is called.
+        """
+        pass
+
+    @abstractmethod
+    def get_action(self, ticks):
+        """
+        Obtain the action designed by this IA.
+        Parameters
+        -----------
+        - *ticks*: (**float**)
+            the maximum ticks that can be used
+
+        Return
+        -----------
+        A tuple containing the direction in which to move, the number of ticks to use,
+         and a boolean indicating if the modifier should be used.
+        type: **Tuple[Direction, float, bool]**
+        """
+        pass
+
+    def update(self, ticks):
+        if not hasattr(self, "init"):
+            self.init_state()
+            self.init = True
+        ticks_used = 1
+        while ticks > 0 or ticks_used > .05:
+            direction, ticks_used, boost_use = self.get_action(ticks)
+            if boost_use:
+                self.entity.use_modifier()
+            self.entity.face(direction)
+            if ticks_used > ticks:
+                ticks_used = ticks
+            self.entity.moving = ticks_used > .05
+            self.game.map.move(self.entity, ticks_used)
+            ticks -= ticks_used
