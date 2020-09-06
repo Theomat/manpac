f3671099cf6462d7875b9afdb7da1634d12c99f0
Theomat <theomatricon@gmail.com>
2020-09-06 14:56:53 +0200

    Added comments

--- manpac/controllers/net/net_client_controller.py
+++ manpac/controllers/net/net_client_controller.py
@@ -12,11 +12,13 @@ import time
 
 
 def _callback_result_(net_client_controller, msg, socket):
+    net_client_controller.has_result = True
     net_client_controller.has_ok = msg.result
 
 
 def _callback_sync_entity_(net_client_controller, msg, socket):
     entity = net_client_controller.game.entities[msg.ent_uid]
+    # Update relevant entity
     entity.teleport(msg.pos)
     entity.face(msg.direction)
     entity.uid = msg.ent_uid
@@ -33,6 +35,7 @@ def _callback_sync_map_(net_client_controller, msg, socket):
 
 
 def _callback_sync_clock_(net_client_controller, msg, socket):
+    net_client_controller.game.duration += net_client_controller.net_ticks
     net_client_controller.net_ticks = msg.ticks - net_client_controller.game.duration
 
 
@@ -43,7 +46,7 @@ def _callback_sync_map_boosts_(net_client_controller, msg, socket):
 
 
 def _callback_end_game_(net_client_controller, msg, socket):
-    net_client_controller.has_game_ended = True
+    net_client_controller.remote_game_status = GameStatus.FINISHED
     net_client_controller.game.status = GameStatus.FINISHED
 
 
@@ -68,7 +71,7 @@ def _callback_your_entity_(net_client_controller, msg, socket):
 
 
 def _callback_start_game_(net_client_controller, msg, socket):
-    net_client_controller.has_game_started = True
+    net_client_controller.remote_game_status = GameStatus.ONGOING
     if net_client_controller.ready_to_start:
         net_client_controller.game.status = GameStatus.ONGOING
 
@@ -101,18 +104,28 @@ _CALLBACKS_ = {
 
 @export
 class NetClientController(AbstractController):
+    """
+    A controller that is a client to a remote server.
+
+    Parameters
+    -----------
+    - *controller*: (**AbstractController**)
+        the actual controller that will control the entity associated with this controller
+    - *host*: (**string**)
+        the host ip
+    - *port*: (**int**)
+        the port of the host
+    """
 
     def __init__(self, controller, host="127.0.0.1", port=9999):
         super(NetClientController, self).__init__(controller.game)
 
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.buffer_size = 4096
-        self.has_started = False
         self.controller = controller
 
         self.has_map = False
-        self.has_game_started = False
-        self.has_game_ended = False
+        self.remote_game_status = GameStatus.NOT_STARTED
         self.ready_to_start = False
 
         self.net_ticks = 0
@@ -124,21 +137,26 @@ class NetClientController(AbstractController):
 
     def on_attach(self, entity):
         super(NetClientController, self).on_attach(entity)
+        # Assign uids
         for i, entity in enumerate(self.game.entities):
             entity.uid = -(i + 1)
 
+        # Bind socket
         ret_code = self.socket.connect_ex((self.host, self.port))
         if not ret_code == 0:
             print("Failed to connect to: host=", self.host, "port=", self.port)
             print("Error code=", ret_code)
             exit()
 
-        server_thread = threading.Thread(target=self._listen_)
-        # Exit the server thread when the main thread terminates
-        server_thread.daemon = True
-        server_thread.start()
-
-        self._notify_(MsgJoin(self.entity.type))
+        # Start listening
+        listen_thread = threading.Thread(target=self._listen_)
+        listen_thread.daemon = True
+        listen_thread.start()
+        # Ask to join
+        result = self._notify_(MsgJoin(self.entity.type))
+        if not result:
+            print("Could not join, no room for", entity.type)
+            exit()
 
     def _listen_(self):
         while not self.socket._closed:
@@ -151,20 +169,23 @@ class NetClientController(AbstractController):
                 _CALLBACKS_[msg.uid](self, msg, self.socket)
 
     def on_game_start(self):
+        # Wait for the map
         while not self.has_map:
             time.sleep(.02)
-        self.controller.on_game_start()
+        # Update local map
         self.game.map.terrain = self.terrain
         self.game.map.boost_generator = None
         self.game.map.compiled = False
         self.game.map.compile()
-
-        if not self.has_game_started:
-            self.game.status = GameStatus.NOT_STARTED
+        # Fire controller event
+        self.controller.on_game_start()
+        # Put in sync with remote game status
+        self.game.status = self.remote_game_status
         self.ready_to_start = True
 
     def on_game_end(self):
-        if not self.has_game_ended:
+        # If remote game has not ended
+        if self.remote_game_status is not GameStatus.FINISHED:
             self.game._fired_end = False
             self.game.status = GameStatus.ONGOING
             return
@@ -172,42 +193,49 @@ class NetClientController(AbstractController):
         self.controller.on_game_end()
 
     def _notify_(self, msg):
+        self.has_result = False
         self.has_ok = False
-        while not self.has_ok:
+        while not self.has_result:
             self._send_message_(msg)
             time.sleep(.1)
-
-    def on_death(self):
-        self._send_message_(MsgSyncEntity(entity=self.entity))
-        self.controller.on_death()
-
-    def on_boost_pickup(self):
-        self.controller.on_boost_pickup()
+        return self.has_ok
 
     def _send_message_(self, msg):
+        """
+        Send the specified NetMessage.
+        Parameters
+        -----------
+        - *msg*: (**NetMessage**)
+            the message to be sent
+        """
         if self.socket._closed:
             self.entity.alive = False
             return
         self.socket.sendall(msg.bytes())
-
-    def _accept_(self, socket, client_address):
-        self.client_address = client_address
-        self.socket = socket
-        self._send_message_(MsgResult(True))
 
     def on_boost_use(self):
         self.controller.on_boost_use()
         self._send_message_(MsgBoostUse(self.entity.uid))
 
     def update(self, ticks):
+        # If we have been updating too much since last update stop
         if self.ticks_since_last_upd >= self.max_ticks_in_advance:
             return
-        self.ticks_since_last_upd += ticks
 
+        # Trigger update of controller if alive
         if self.entity.alive:
             self.controller.update(ticks)
-        self._send_message_(MsgSyncEntity(entity=self.entity))
-        if self.ticks_since_last_upd > ticks:
+            self._send_message_(MsgSyncEntity(entity=self.entity))
+        # If more than on tick since last update then emulate movement
+        if self.ticks_since_last_upd > 0:
             for entity in self.game.entities:
                 if entity != self.entity:
                     self.game.map.move(entity, ticks)
+
+        self.ticks_since_last_upd += ticks
+
+    def on_death(self):
+        self.controller.on_death()
+
+    def on_boost_pickup(self):
+        self.controller.on_boost_pickup()
